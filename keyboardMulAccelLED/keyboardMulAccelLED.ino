256d426954e9043ba9ef16635a8c0dd39d4a1b5e
firmread <litchirhythm@gmail.com>
2015-05-02 19:59:25 -0400

    working single mpr121, noted multiple workaround in readme

--- keyboardMulAccelLED/keyboardMulAccelLED.ino
+++ keyboardMulAccelLED/keyboardMulAccelLED.ino
@@ -5,6 +5,7 @@
 #include <Wire.h>
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
+#include "Adafruit_MPR121.h"
 
 
 // How many NeoPixels are attached to the Arduino?
@@ -39,7 +40,18 @@ boolean bDidType = false;
 #define THRESH 500
 float smoothed[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
+
+//MPR121
+// You can have up to 4 on one i2c bus but one is enough for testing!
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+// Keeps track of the last pins touched
+// so we know when buttons are 'released'
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
 void setup() {
+  while (!Serial); 
 
   Wire.begin();
   Wire.beginTransmission(MPU);
@@ -50,6 +62,21 @@ void setup() {
   //  pinMode(buttonPin, INPUT);
   //while (!Serial)
   Serial.begin(115200);
+  
+  
+  //MPR121
+  Serial.println("Adafruit MPR121 Capacitive Touch sensor test"); 
+  
+  // Default address is 0x5A, if tied to 3.3V its 0x5B
+  // If tied to SDA its 0x5C and if SCL then 0x5D
+  if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
+  
+  
+  
   Keyboard.begin();
   pixels.begin(); // This initializes the NeoPixel library.
 }
@@ -150,6 +177,44 @@ void loop() {
     }
     delay(5);
   }
+  
+  
+  
+  // Get the currently touched pads
+  currtouched = cap.touched();
+  
+  for (uint8_t i=0; i<12; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" touched");
+    }
+    // if it *was* touched and now *isnt*, alert!
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" released");
+    }
+  }
+
+  // reset our state
+  lasttouched = currtouched;
+
+  // comment out this line for detailed data from the sensor!
+  return;
+  
+  // debugging info, what
+  Serial.print("\t\t\t\t\t\t\t\t\t\t\t\t\t 0x"); Serial.println(cap.touched(), HEX);
+  Serial.print("Filt: ");
+  for (uint8_t i=0; i<12; i++) {
+    Serial.print(cap.filteredData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  Serial.print("Base: ");
+  for (uint8_t i=0; i<12; i++) {
+    Serial.print(cap.baselineData(i)); Serial.print("\t");
+  }
+  Serial.println();
+  
+  // put a delay so it isn't overwhelming
+  delay(100);
 }
 
 
--- readme.md
+++ readme.md
@@ -9,5 +9,7 @@
 	* https://learn.adafruit.com/plush-game-controller/the-code
 	* https://github.com/adafruit/adafruit_flora_game_controller
 * MPR121 tutorial https://learn.adafruit.com/adafruit-mpr121-12-key-capacitive-touch-sensor-breakout-tutorial/wiring
+	* **To hook up multiple MPR121** 
+		* ADDR is the I2C address select pin. By default this is pulled down to ground with a 100K resistor, for an I2C address of 0x5A. You can also connect it to the 3Vo pin for an address of 0x5B, the SDA pin for 0x5C or SCL for address 0x5D 
 * MPU-6050 Short example sketch http://playground.arduino.cc/Main/MPU-6050#short
 * MPU-6050 i2c libs **not using just for ref** https://github.com/jrowberg/i2cdevlib/tree/master/Arduino/MPU6050
