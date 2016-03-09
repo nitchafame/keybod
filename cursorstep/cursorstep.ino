059572afe20b00c2b0edce47903e5c4a484e7cca
firmread <litchirhythm@gmail.com>
2016-03-09 01:19:23 -0500

    move smooth to its own thing

--- cursorstep/cursorstep.ino
+++ cursorstep/cursorstep.ino
@@ -49,10 +49,6 @@ void loop() {
   Serial.print(" | GyZ = "); Serial.println(GyZ);
 
 
-  //mouseMove();
-
-  //Serial.print(reading);
-
   //-----Actions - Click
 
   if (AcX > 10000) {
@@ -94,18 +90,3 @@ void loop() {
   delay(103);
 }
 
-//----- Simple lowpass filter
-// requires recycling the output in the "smoothedVal" param
-
-//int smooth(int data, float filterVal, float smoothedVal) {
-//
-//  if (filterVal > 1) {     // check to make sure param's are within range
-//    filterVal = .999999;
-//  }
-//  else if (filterVal <= 0) {
-//    filterVal = 0;
-//  }
-//  smoothedVal = (data * (1 - filterVal)) + (smoothedVal  *  filterVal);
-//  return (int)smoothedVal;
-//  delay(333);
-//}
