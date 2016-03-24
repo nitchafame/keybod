90553c3acf4d5f21f028ed580c4185b8a64d849e
firmread <litchirhythm@gmail.com>
2016-03-24 09:23:46 -0400

    small fix for readability

--- neckCollar/LBB_testAnalog2/LBB_testAnalog2.ino
+++ neckCollar/LBB_testAnalog2/LBB_testAnalog2.ino
@@ -1,24 +1,26 @@
-int prevAnalog0;
+int prevA0;
+int smoothedA0;
+
+float speed = 0.1;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int rawAnalog0 = analogRead(A0);
+  // get zeno smoothed A0
+  smoothedA0 = prevA0 * (1 - speed) + analogRead(A0) * speed;
+  Serial.println(smoothedA0);
 
-  // zeno smooth
-  float speed = 0.1;
-  int analog0 = prevAnalog0 * (1 - speed) + rawAnalog0 * speed;
-  Serial.println(analog0);
-
-  int mappedForLed = map(analog0, 700, 1000, 0, 255);
+  // map smoothedA0 to LED
+  int mappedForLed = map(smoothedA0, 700, 1000, 0, 255);
   Bean.setLed(mappedForLed, 0, 255 - mappedForLed);
+  //red for high value
+  //green for low value
+  //what does high vs low means? which one is looking down?
 
-  prevAnalog0 = analog0;
+  // update for next loop
+  prevA0 = smoothedA0;
 
   //  Bean.sleep(100);
-
 }
